4beb115f314462b84355f8b5e77342a43bbbea8c
SerhiyStepanov <SerhiyStepanov@users.noreply.github.com>
2020-09-25 12:38:17 +0300

    add svg

--- js/gallery.js
+++ js/gallery.js
@@ -1,7 +1,7 @@
 import gallery from "./js/gallery-items";
 
 const murkUpGallery = ({ preview, original, description }) => {
-  console.log();
+  console.log(gallery[0]);
 };
 
 murkUpGallery(gallery);
