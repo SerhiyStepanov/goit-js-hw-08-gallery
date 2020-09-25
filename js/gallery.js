5358b7a6c2087da9d9093469a8a727e23b94bb64
SerhiyStepanov <SerhiyStepanov@users.noreply.github.com>
2020-09-25 14:09:13 +0300

    add-markup

--- js/gallery.js
+++ js/gallery.js
@@ -1,3 +1,27 @@
 import gallery from "./gallery-items.js";
 
-console.log(gallery);
+const markupGalleryCart = ({ original, preview, description }) => {
+  return `<li class="gallery__item">
+  <a
+    class="gallery__link"
+    href="${original}"
+  >
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>`;
+};
+
+// console.log(gallery);
+
+const galleryImg = document.querySelector(".gallery");
+// console.log(galleryImg);
+const markupGallery = gallery.map(markupGalleryCart).join("");
+// console.log(markupGallery);
+galleryImg.insertAdjacentHTML("afterbegin", markupGallery);
+
+console.log(galleryImg);
