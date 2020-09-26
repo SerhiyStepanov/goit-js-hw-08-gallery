abb904f61d9460bcbae7c582c2d604042390fa7f
SerhiyStepanov <SerhiyStepanov@users.noreply.github.com>
2020-09-26 19:28:05 +0300

    refactoring

--- js/gallery.js
+++ js/gallery.js
@@ -35,40 +35,49 @@ const imageEl = document.querySelector(".gallery__image");
 // console.log(imageEl);
 
 galleryImage.addEventListener("click", onOpenModal);
-closeModalbtn.addEventListener("click", onCloseModal);
-backdrop.addEventListener("click", onClickBackdrop);
+closeModalbtn.addEventListener("click", onCloseModalBtn);
+backdrop.addEventListener("click", onCloseModalBackdrop);
 
 function onOpenModal(event) {
   event.preventDefault();
   if (event.target.nodeName !== "IMG") {
     return;
   }
-  window.addEventListener("keydown", onPressEscape);
+  window.addEventListener("keydown", onCloseModalEscape);
   backdrop.classList.add("is-open");
   imgOpenInModal.src = event.target.dataset.source;
 }
 
-function onCloseModal(event) {
+function closeModal(event) {
+  backdrop.classList.remove("is-open");
+  imgOpenInModal.src = "";
+  window.removeEventListener("keydown", onCloseModalEscape);
+}
+
+function onCloseModalBtn(event) {
   if (event.target.nodeName !== "BUTTON") {
     return;
   }
-  backdrop.classList.remove("is-open");
-  imgOpenInModal.src = "";
-  window.removeEventListener("keydown", onPressEscape);
+  closeModal();
+  // backdrop.classList.remove("is-open");
+  // imgOpenInModal.src = "";
+  // window.removeEventListener("keydown", onCloseModalEscape);
 }
 
-function onClickBackdrop(event) {
+function onCloseModalBackdrop(event) {
   if (event.target.classList.contains("lightbox__overlay")) {
-    backdrop.classList.remove("is-open");
-    imgOpenInModal.src = "";
-    window.removeEventListener("keydown", onPressEscape);
+    closeModal();
+    // backdrop.classList.remove("is-open");
+    // imgOpenInModal.src = "";
+    // window.removeEventListener("keydown", onCloseModalEscape);
   }
 }
 
-function onPressEscape(event) {
+function onCloseModalEscape(event) {
   if (event.code === "Escape") {
-    backdrop.classList.remove("is-open");
-    imgOpenInModal.src = "";
-    window.removeEventListener("keydown", onPressEscape);
+    closeModal();
+    // backdrop.classList.remove("is-open");
+    // imgOpenInModal.src = "";
+    // window.removeEventListener("keydown", onCloseModalEscape);
   }
 }
